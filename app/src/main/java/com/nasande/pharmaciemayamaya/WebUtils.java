5349ca1af5ea8ab00906bc81fedcb249837718bd
bossygit <bienvenu.kitutu@gmail.com>
2020-06-09 12:02:56 +0200

    Assets folder style

---
+++ app/src/main/java/com/nasande/pharmaciemayamaya/WebUtils.java
@@ -0,0 +1,9 @@
+package com.nasande.pharmaciemayamaya;
+
+import android.util.Base64;
+
+import java.io.InputStream;
+
+public class WebUtils {
+
+}
--- app/src/main/java/com/nasande/pharmaciemayamaya/ui/home/HomeFragment.java
+++ app/src/main/java/com/nasande/pharmaciemayamaya/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.nasande.pharmaciemayamaya.ui.home;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,10 +18,13 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.nasande.pharmaciemayamaya.R;
 
+import java.io.InputStream;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     public WebView mWebView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,10 +32,49 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         mWebView = (WebView)root.findViewById(R.id.webview_home);
-        mWebView.setWebViewClient(new WebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                // Inject CSS when page is done loading
+                injectCSS();
+                super.onPageFinished(view, url);
+            }
+        });
+
         mWebView.loadUrl("http://mayamaya.nasande.cg");
 
+
+
         return root;
+    }
+
+    private void injectCSS() {
+        if(getActivity()!=null){
+
+
+            try {
+                InputStream inputStream = getActivity().getAssets().open("style.css");
+
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                inputStream.close();
+                String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+                mWebView.loadUrl("javascript:(function() {" +
+                        "var parent = document.getElementsByTagName('head').item(0);" +
+                        "var style = document.createElement('style');" +
+                        "style.type = 'text/css';" +
+                        // Tell the browser to BASE64-decode the string into your script !!!
+                        "style.innerHTML = window.atob('" + encoded + "');" +
+                        "parent.appendChild(style)" +
+                        "})()");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }
