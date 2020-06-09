45deaea332f7f8506990433815cf143005593c0d
bossygit <bienvenu.kitutu@gmail.com>
2020-06-09 05:57:49 +0200

    Home page set

--- app/src/main/java/com/nasande/pharmaciemayamaya/ui/home/HomeFragment.java
+++ app/src/main/java/com/nasande/pharmaciemayamaya/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,19 +19,18 @@ import com.nasande.pharmaciemayamaya.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    public WebView mWebView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        mWebView = (WebView)root.findViewById(R.id.webview_home);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://mayamaya.nasande.cg");
+
         return root;
     }
 }
