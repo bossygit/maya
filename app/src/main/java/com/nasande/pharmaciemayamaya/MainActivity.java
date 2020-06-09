bd0076b3bcafe6e70bf787ae96489deea1ff5f3e
bossygit <bienvenu.kitutu@gmail.com>
2020-06-09 18:44:48 +0200

    remove actionbar

--- app/src/main/java/com/nasande/pharmaciemayamaya/MainActivity.java
+++ app/src/main/java/com/nasande/pharmaciemayamaya/MainActivity.java
@@ -29,8 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -50,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
