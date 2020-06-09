fd9a8c9ef027af543ed5bef171545b0b065cbbea
bossygit <bienvenu.kitutu@gmail.com>
2020-06-09 15:35:13 +0200

    webview info & product

--- app/src/main/java/com/nasande/pharmaciemayamaya/ui/ContactFragment.java
+++ app/src/main/java/com/nasande/pharmaciemayamaya/ui/ContactFragment.java
@@ -8,15 +8,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.nasande.pharmaciemayamaya.R;
+
+import java.io.InputStream;
 
 public class ContactFragment extends Fragment {
 
     private ContactViewModel mViewModel;
+    public WebView mWebView;
 
     public static ContactFragment newInstance() {
         return new ContactFragment();
@@ -25,7 +31,24 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contact_fragment, container, false);
+        View root = inflater.inflate(R.layout.contact_fragment, container, false);
+
+        mWebView = (WebView)root.findViewById(R.id.webview_contact);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                // Inject CSS when page is done loading
+                injectCSS();
+                super.onPageFinished(view, url);
+            }
+        });
+
+        mWebView.loadUrl("http://mayamaya.nasande.cg/contact");
+
+        return root;
     }
 
     @Override
@@ -33,6 +56,33 @@ public class ContactFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void injectCSS() {
+        if(getActivity()!=null){
+
+
+            try {
+                InputStream inputStream = getActivity().getAssets().open("style.css");
+
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                inputStream.close();
+                String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+                mWebView.loadUrl("javascript:(function() {" +
+                        "var parent = document.getElementsByTagName('head').item(0);" +
+                        "var style = document.createElement('style');" +
+                        "style.type = 'text/css';" +
+                        // Tell the browser to BASE64-decode the string into your script !!!
+                        "style.innerHTML = window.atob('" + encoded + "');" +
+                        "parent.appendChild(style)" +
+                        "})()");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
 }
